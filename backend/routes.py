941c0378a86b982e932f47fc36c8bfbdc4ec6ddc
dTravis23 <datra5@morgan.edu>
2026-06-30 00:56:52 -0400

    - Added more CRUD operations - Finalized POST and DELETE Methods - Swapped query params for route-based approach - Added list_position column - Implemented logic to shift list placements - Swapped ID-based routes for list position-based routes

--- backend/routes.py
+++ backend/routes.py
@@ -40,49 +40,113 @@ class Level(SQLModel, table=True):
     creator: str | None
     first_victor: str | None
     completion_link: str | None
-    
+    list_position: int | None = Field(default=1, ge=1, unique=True)
+
+
 class LevelCreate(SQLModel):
-    level_name: str
-    creator: str
-    first_victor: str
-    completion_link: str
+    level_name: str | None
+    creator: str | None
+    first_victor: str | None
+    completion_link: str | None
+    list_position: int | None =  Field(default=1, ge=1, unique=True)
+
 
 # Generic Response Model
 T = TypeVar("T")
+
+
 class ResponseModel(BaseModel, Generic[T]):
     data: T
 
+
 app = FastAPI(root_path="/api/v1", lifespan=lifespan)
 
+
 @app.get("/")
-def root():
+async def root():
     return {"Status: Nox List API is currently active!"}
 
 
 @app.get("/levels", response_model=ResponseModel[list[Level]])
-def read_levels(session : SessionDep):
-    
-    all_levels = session.exec(
-        select(Level).order_by(Level.level_id) # type: ignore
-        ).all()
-    print(all_levels)
-    
+async def read_levels(session: SessionDep):
+
     try:
+
+        all_levels = session.exec(
+            select(Level).order_by(Level.list_position)  # type: ignore
+        ).all()
+        print(all_levels)
+
         return {"data": all_levels}
+
     except:
         raise HTTPException(status_code=404, detail="There are no levels to display")
-    
-@app.post("/levels", response_model=Level)
-def create_level(level_id: int, level: LevelCreate, session: SessionDep):
+
+
+@app.post("/levels/", response_model=ResponseModel[LevelCreate])
+async def create_level(level: LevelCreate, session: SessionDep):
+
+    try:
         level_add = Level.model_validate(level)
+
+        all_levels = session.exec(
+            select(Level)
+            .where(Level.list_position >= level_add.list_position)
+            .order_by(Level.list_position.desc())  # type: ignore
+        ).all()
+
+        for lev in all_levels:
+            lev.list_position += 1  # pyright: ignore[reportGeneralTypeIssues, reportAttributeAccessIssue]
+
+        all_levels = session.exec(select(Level).order_by(Level.list_position)).all() # type: ignore
+
         session.add(level_add)
         session.commit()
         session.refresh(level_add)
-        
-        return {"data" : level_add}
-    
-@app.patch("/levels?id=", response_model=ResponseModel[Level])
-def update_level(level: Level, session: SessionDep):
-    updated_level = session.get(Level, Level.level_id)
-    return ""
-    
+
+        return {"data": level_add}
+    except:
+        print("Something went wrong.")
+
+
+@app.get("/levels/{pos}", response_model=ResponseModel[Level])
+async def read_level_at_id(session: SessionDep, pos: int):
+
+    try:
+
+        this_level = session.exec(select(Level).where(Level.list_position == pos)).first()
+
+        return {"data": this_level}
+
+    except:
+        raise HTTPException(status_code=404, detail=f"Level with ID {id} not found!")
+
+
+@app.delete("/levels/{id}", response_model=ResponseModel[list[Level]])
+async def delete_level_at_id(session: SessionDep, id: int):
+
+    try:
+        this_level = session.get(Level, id)
+        session.delete(this_level)
+        session.commit()
+
+        all_levels = session.exec(select(Level)).all()
+
+        return {"data": all_levels}
+
+    except:
+        raise HTTPException(status_code=404, detail=f"Level with ID {id} not found!")
+
+
+@app.delete("/levels/", response_model=ResponseModel[list[Level]])
+async def delete_all_levels(session: SessionDep):
+
+    all_tasks = session.exec(select(Level)).all()
+
+    for lev in all_tasks:
+        session.delete(lev)
+
+        session.commit()
+        session.refresh(Level)
+
+    return {"data": all_tasks}
